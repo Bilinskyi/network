46c368242e627f56c518ca0a8de06c66938632ac
Taras Bilinskyi <1unitedcrew@gmail.com>
2016-09-19 08:56:33 +0300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,6 @@ gulp.task('ftp', function () {
         }));
 });
 
-
 gulp.task('watch', ['sass', 'browserSync'], function () {
 	gulp.watch('app/sass/**/*.scss', ['sass']);
 // Reloads the browser whenever HTML or JS files change
